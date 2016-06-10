ba509401fccbfe8b6a13e705a250d234c8a2a01a
jv3322 <jv3322skyline@umich.edu>
2016-06-09 21:28:23 -0400

    cos uses radians

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -7,9 +7,9 @@ const double ticksPerSecond = 44.0;
 const double forwardDrive = 4.0;
 const double afterTurnForwardDrive = 2.0;
 const double turnAngle = 90.0;
-const double liftDown = 3.7;
-const double liftBack = .91;
-const double liftVertical = 2.26;
+const double liftForward = 4.31;
+const double liftBack = 1.55;
+const double liftVertical = 2.62;
 
 class Robot: public IterativeRobot
 {
@@ -35,15 +35,15 @@ class Robot: public IterativeRobot
 public:
 	Robot() :
 		liftPot(0),
-		liftController(0.2, 0.0, 0.0, &liftPot, &lift),
+		liftController(0.05, 0.005, 0.05, &liftPot, &lift),
 		ultrasonic(1),
 		driveEncoder(0, 1, false, Encoder::k4X),
         table(NULL),
 		shooter(5),
 		lift(4),
 		myRobot(2,3,0,1),
-		driveStick(0),
-		techStick(1),
+		driveStick(1),
+		techStick(0),
         ahrs(NULL),
 		lw(LiveWindow::GetInstance()),
 		driveFast(false),
@@ -98,7 +98,7 @@ private:
 	}
 	void AutonomousPeriodic()
 	{
-		SmartDash(0);
+		SmartDash(0, 0);
 		timer++;
 
 		// lower lift
@@ -158,40 +158,39 @@ private:
 
 	void TeleopPeriodic()
 	{
-		timer++;
-
-
-
 		double liftAngle;
-		double liftPotSum = 0;
 
-		for (int i = 0; i < 10; i++){
-			liftPotSum+=liftPot.GetVoltage();
+		// compute average of several liftPot samples
+		double liftPotTotal;
+		int liftPotCount;
+		for (liftPotCount = 0; liftPotCount < 50; liftPotCount++) {
+			liftPotTotal += liftPot.GetVoltage();
 		}
-		double liftPotAverage = liftPotSum / 10.0;
-		liftAngle = (liftPotAverage-liftDown)*90.0/(liftVertical-liftDown);
-		liftAngle = 0;
+		double liftPotAverage = liftPotTotal / liftPotCount;
+		liftAngle = (liftPotAverage-liftForward)*3.1415/2.0/(liftVertical-liftForward);
+		// liftAngle = 45;
 		lift.Set(-0.3 * cos(liftAngle));
-
 
 /*
 		// Control the arm automatically
 		if (techStick.GetRawButton(2)) { // B
+			liftController.Disable();
 			liftController.Enable();
-			liftController.SetSetpoint(liftDown);
+			liftController.SetSetpoint(liftForward);
 		}
 		if (techStick.GetRawButton(4)) { // Y
+			liftController.Disable();
 			liftController.Enable();
 			liftController.SetSetpoint(liftVertical);
 		}
 		if (techStick.GetRawButton(3)) { // X
+			liftController.Disable();
 			liftController.Enable();
 			liftController.SetSetpoint(liftBack);
 		}
 		if (techStick.GetRawButton(10)) { // push down right stick
 			liftController.Disable();
 		}
-*/
 
 		// Control the shooter
 		if (techStick.GetRawAxis(2)) {
@@ -209,7 +208,7 @@ private:
 			shooterDefaultOn = !shooterDefaultOn;
 		}
 		lastShooterToggleButton = techStick.GetRawButton(8);
-
+*/
 /*
 		// Control the arm manually
 		if (techStick.GetRawButton(5)) { // LB
@@ -223,7 +222,7 @@ private:
 */
 
 		// Control the drive speed
-		if (!driveToggleButton && driveStick.GetRawButton(1)) {
+		if (!driveToggleButton && driveStick.GetRawButton(1)) { // A
 			driveFast = !driveFast;
 			// myRobot.SetMaxOutput(driveFast ? 0.6 : 0.3);
 		}
@@ -241,8 +240,7 @@ private:
 		//myRobot.TankDrive(driveStick.GetRawAxis(1),driveStick.GetRawAxis(5));
 		//myRobot.ArcadeDrive(driveStick); // drive with arcade style (use right stick)
 
-		SmartDash(liftPotAverage);
-
+		SmartDash(liftPotAverage, liftAngle);
 	}
 	void TestPeriodic()
 	{
@@ -255,18 +253,20 @@ private:
 		angleError = angleError*.02;
 		myRobot.Drive(outputMagnitude, -angleError);
 	}
-	void SmartDash(double liftPotAverage)
+	void SmartDash(double liftPotAverage, double liftAngle)
 	{
 		SmartDashboard::PutNumber("IMU Yaw", GetAngle());
 		SmartDashboard::PutNumber("Encoder Distance", driveEncoder.GetDistance());
 		SmartDashboard::PutNumber("Encoder Rate", driveEncoder.GetRate());
-		SmartDashboard::PutNumber("Lift Potentiometer Voltage", liftPot.GetVoltage());
+		SmartDashboard::PutNumber("Lift Potentiometer Voltage (average)", liftPotAverage);
+		SmartDashboard::PutNumber("Lift Angle", liftAngle);
+		SmartDashboard::PutNumber("Lift Power", -0.3 * cos(liftAngle));
+		SmartDashboard::PutNumber("Cos(Pi)", cos(3.1415));
+
+
 		SmartDashboard::PutNumber("Ultrasonic", ultrasonic.GetValue());
 		SmartDashboard::PutNumber("Speed", driveStick.GetRawAxis(1));
 		SmartDashboard::PutNumber("Turn", driveStick.GetRawAxis(4));
-		double liftAngle;
-		liftAngle = (liftPotAverage-liftDown)*90.0/(liftVertical-liftDown);
-		SmartDashboard::PutNumber("Lift Angle", liftAngle);
 	}
 	float GetAngle()
 	{
